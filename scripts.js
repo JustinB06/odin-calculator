17879a127d3eb01d1c12b2dfaaa6fedac5a686ff
JustinB06 <191050741+JustinB06@users.noreply.github.com>
2025-11-19 18:01:47 -0500

    Implement operate() function
    The function will delegate a calculator operation
    to be handled by a specific operation function.

--- scripts.js
+++ scripts.js
@@ -1,3 +1,12 @@
+/* An operation will be made up of: num1, 
+   operator, then num2. 
+   
+   Three variables that store the parts of 
+   the operation: 
+*/
+let num1, operator, num2;
+
+/* 4 functions to perform the basic operations */
 function add(num1, num2) {
   return num1 + num2;
 }
@@ -12,4 +21,21 @@ function multiply(num1, num2) {
 
 function divide(num1, num2) {
   return num1 / num2;
+}
+
+function operate(num1, operator, num2) {
+  switch (operator) {
+    case "+":
+      return add(num1, num2);
+    //   break;
+    case "-":
+      return subtract(num1, num2);
+    //   break;
+    case "*":
+      return multiply(num1, num2);
+    //   break;
+    case "/":
+      return divide(num1, num2);
+    //   break;
+  }
 }
